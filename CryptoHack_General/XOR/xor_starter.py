b844b6597908ce38895beebb3b1d17a1e983939d
ryanmcarr99 <ryanmcarr1999@gmail.com>
2020-11-13 02:39:31 +0000

    xor

---
+++ CryptoHack_General/XOR/xor_starter.py
@@ -0,0 +1,16 @@
+# String = label
+c1 = 108
+c2 = 97
+c3 = 98
+c4 = 101
+c5 = 108
+
+x = 13
+
+print(13 ^ c1)
+print(13 ^ c2)
+print(13 ^ c3)
+print(13 ^ c4)
+print(13 ^ c5)
+
+#output = 97, 108, 111, 104, 97 = aloha

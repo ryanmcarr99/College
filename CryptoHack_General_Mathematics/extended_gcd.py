db645611dbbfae9328d97bd065f9bde70536ea12
ryanmcarr99 <ryanmcarr1999@gmail.com>
2021-01-08 21:41:24 +0000

    commit

---
+++ CryptoHack_General_Mathematics/extended_gcd.py
@@ -0,0 +1,10 @@
+from fractions import gcd
+
+p = 26513
+
+q = 32321
+
+#print (gcd(p,q))
+#gcd(p,q) = 1
+
+# p*u + q*v = 1
---
+++ CryptoHack_General_Mathematics/greatest_common_divisor.py
@@ -0,0 +1,7 @@
+from fractions import gcd
+
+a = 66528
+
+b = 52920
+
+print (gcd(a,b))

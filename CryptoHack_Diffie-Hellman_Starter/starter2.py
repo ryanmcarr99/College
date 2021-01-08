df3f460c31a43f7c74aa5a5b3c4110d0fa1a12bd
ryanmcarr99 <ryanmcarr1999@gmail.com>
2021-01-08 21:07:14 +0000

    commit

---
+++ CryptoHack_Diffie-Hellman_Starter/starter2.py
@@ -0,0 +1,13 @@
+p = 28151
+g = 2
+
+found = False
+
+while not found: 
+    for n in range(2,p):
+        if pow(g,n,p) == 1:
+            break
+        if n == p-2:
+            print (g)
+            found =True
+    g=g+1
